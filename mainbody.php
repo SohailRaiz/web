503337b3537f9d316984e4abd669faaf6ae939e6
SohailRaiz <12205013@gift.edu.pk>
2015-06-25 14:57:43 +0500

    Create mainbody.php

---
+++ mainbody.php
@@ -0,0 +1,24 @@
+<?php	
+$query = "select * from userposts order by rand() LIMIT 0,4";
+$run= mysql_query($query);
+while($row=mysql_fetch_array($run)){
+
+	$post_id = $row['post_id']; 
+	$title = $row['post_name'];
+	$post_author = $row['post_author'];
+	$post_content =$row['post_content'];
+?>
+<h1>
+ <?php echo $title; ?>
+</h1>
+
+<p id="author">Posted by:<b><?php echo $post_author; ?></b>
+</p>
+
+<p id="content"><?php echo $post_content; ?></p>
+
+<hr>
+
+<?php 
+} 
+?>
